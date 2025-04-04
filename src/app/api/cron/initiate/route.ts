74da1c2339cfbdc6e406ca0442bd3a943455f2cc
Misty-clouds <adenijitaoheed26@gmail.com>
2025-04-04 20:04:43 +0100

    cron initiate fixed

--- src/app/api/cron/initiate/route.ts
+++ src/app/api/cron/initiate/route.ts
@@ -4,7 +4,7 @@ import axios from 'axios';
 export async function GET() {
   try {
     // Call the process-withdrawals API route using axios
-    const response = await axios.get('https://53e2-185-177-124-101.ngrok-free.app/api/cron/process-withdrawals');
+    const response = await axios.get(`${process.env.NEXT_PUBLIC_APP_URL_TEST}/api/cron/process-withdrawals`);
 
     const result = response.data;
     return NextResponse.json({ message: 'Cron job executed successfully', result },{status:200});
