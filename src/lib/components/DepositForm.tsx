e1718db3a44ef1087aba57742b6775c97e4ec997
Misty-clouds <adenijitaoheed26@gmail.com>
2025-04-14 19:21:16 +0100

    fixed level for from 8 to 4

--- src/lib/components/DepositForm.tsx
+++ src/lib/components/DepositForm.tsx
@@ -24,7 +24,7 @@ export default function DepositPage() {
     { label: "Level 1: $80", value: 80, dailyProfit: 2 },
     { label: "Level 2: $100", value: 100, dailyProfit: 3 },
     { label: "Level 3: $200", value: 200, dailyProfit: 6 },
-    { label: "Level 4: $400", value: 400, dailyProfit: 8 },
+    { label: "Level 4: $400", value: 400, dailyProfit: 12 },
     { label: "Level 5: $500", value: 500, dailyProfit: 15 },
     { label: "Level 6: $1,000", value: 1000, dailyProfit: 30 },
     { label: "Level 7: $2,000", value: 2000, dailyProfit: 60 },
